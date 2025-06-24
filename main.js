1093de8a066c02ff245cea3eafc66725e586196f
TechnikTil <til.tjardes@gmail.com>
2025-06-23 21:55:28 -0600

    remake main.js again because it hates me????

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-async function getRandomGreeting()
+const randomGreetings = [
+	"HEY EVERY !",
+	"Ello!",
+	"Oh, hi!",
+	"Salutations.",
+	"Hello!",
+	"Hello Everyone!",
+	"Hi!",
+	"Hello Person!"
+];
+
+function getRandomGreeting()
 {
-	const randomGreetingsResponse = await fetch('./assets/data/randomGreeting.txt');
-	const randomGreetingsFile = await randomGreetingsResponse.text();
-	const randomGreetings = randomGreetingsFile.split('\n');
-	var randomGreeting = randomGreetings[Math.floor(Math.random() * randomGreetings.length)];
+	const randomGreeting = randomGreetings[Math.floor(Math.random() * randomGreetings.length)];
 	return randomGreeting;
 }
 
-getRandomGreeting().then(value => {
-	// gonna do most stuff here, because here is when most html stuff is initialized
-	console.log(value);
+document.body.onload = () => {
+	var greeting = getRandomGreeting();
+	console.log(greeting);
 	const randomGreeting = document.getElementById('randomGreeting');
 	if(randomGreeting != null)
-		randomGreeting.innerText = value;
+		randomGreeting.innerText = greeting;
 
 	const stuffAlright = document.getElementById('stuffAlright');
 	if(Math.random() <= 0.1 && stuffAlright != null)
 		stuffAlright.innerText = 'Shit'; // hehehe im so funny hehehehe
-});
+}
