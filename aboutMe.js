56eb67279e59e6d1b767b57602634bc3bd278e4b
TechnikTil <techniktil@tilnotdrip.org>
2025-10-13 20:14:11 -0600

    rework date stuff

--- aboutMe.js
+++ aboutMe.js
@@ -1,64 +1,57 @@
-const tilTimeZone = -6;
+const tilTimeZone = 'America/Edmonton';
+const tilBirthday = Date.UTC(2010, 8, 24);
+console.log(tilBirthday);
 
 function setTilDate()
 {
+	const tilTime = document.getElementById('tilTime');
+
 	const date = new Date(Date.now());
-	date.setUTCHours(date.getUTCHours() + tilTimeZone);
 
 	let milisecondsElapsed = date.getUTCMilliseconds();
 
-	const tilTime = document.getElementById('tilTime');
 	if(tilTime != null)
-		tilTime.innerText = formatTime(date);
+	{
+		tilTime.innerText = date.toLocaleTimeString('en-US', {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZone: tilTimeZone
+		});
+	}
+
 	setTimeout(function() {
 		setTilDate();
 	}, 1000 - milisecondsElapsed);
 }
 
+function setTilAge()
+{
+	const tilAge = document.getElementById('tilAge');
+	const tilBirthdayDocument = document.getElementById('tilBirthDate');
+
+	const tilBirthDate = new Date(tilBirthday);
+
+	if(tilBirthDate != null)
+	{
+		tilBirthdayDocument.innerText = tilBirthDate.toLocaleDateString('en-CA', {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			timeZone: "UTC"
+		});
+	}
+
+	const timeSinceDate = new Date();
+	timeSinceDate.setTime(Date.now() - tilBirthDate.getTime());
+
+	if(tilAge != null)
+	{
+		tilAge.innerText = (timeSinceDate.getUTCFullYear() - 1970);
+	}
+}
+
 window.addEventListener('load', () => {
 	setTilDate();
+	setTilAge();
 });
-
-function formatTime(date)
-{
-	// formatting
-	let formattedTime = "";
-	let isPM = false;
-
-	if(date.getUTCHours() > 12)
-		isPM = true;
-
-	if(isPM)
-		formattedTime += date.getUTCHours() - 12;
-	else
-		formattedTime += date.getUTCHours();
-
-	formattedTime += ":";
-
-	let formattedMinutes = String(date.getUTCMinutes());
-
-	while(formattedMinutes.length < 2)
-	{
-		formattedMinutes = "0" + formattedMinutes;
-	}
-
-	formattedTime += formattedMinutes;
-
-	formattedTime += ":";
-
-	let formattedSeconds = String(date.getUTCSeconds());
-
-	while(formattedSeconds.length < 2)
-	{
-		formattedSeconds = "0" + formattedSeconds;
-	}
-
-	formattedTime += formattedSeconds;
-
-	if(isPM)
-		formattedTime += " PM";
-	else
-		formattedTime += " AM";
-
-	return formattedTime;
-}
