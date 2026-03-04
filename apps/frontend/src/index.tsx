940bafd958bde2e75c35e666037481d72c524dee
TechnikTil <techniktil@tilnotdrip.org>
2026-03-04 14:02:03 -0700

    Change URL on Navigation
    Started around 1772655300, WakaTime reports 1 hour of Coding.
    
    Signed-off-by: TechnikTil <techniktil@tilnotdrip.org>

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
+import { BrowserRouter } from "react-router-dom";
 import NavigationBar from "./NavigationBar.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<NavigationBar />
+		<BrowserRouter>
+			<NavigationBar />
+		</BrowserRouter>
 	</StrictMode>,
 );
