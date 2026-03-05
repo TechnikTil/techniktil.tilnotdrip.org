36b4c5bb45c9adbe76971076b1aa947308f38358
TechnikTil <techniktil@tilnotdrip.org>
2026-03-04 19:18:49 -0700

    About Me implemented!
    Started at 1772671533.
    
    Signed-off-by: TechnikTil <techniktil@tilnotdrip.org>

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,9 +8,23 @@ const PORT: number = 5000;
 app.use(cors());
 app.use(express.json());
 
+// TODO: move these into seperate modules later.
+
 app.get("/ping", (req: Request, res: Response) =>
 {
 	res.json({message: "Pong!", timestamp: new Date().toISOString()});
+});
+
+app.get("/timezone", (req: Request, res: Response) =>
+{
+	const date: Date = new Date();
+
+	const timezoneName: string =
+		new Intl.DateTimeFormat("en-CA", {timeZoneName: "long"}).formatToParts(new Date()).find(part =>
+			part.type === "timeZoneName"
+		)?.value || "Unknown???? :whattheshit:";
+
+	res.json({name: timezoneName, offset: date.getTimezoneOffset(), timestamp: date.toISOString()});
 });
 
 app.listen(PORT, () =>
--- apps/frontend/src/pages/AboutMePage.tsx
+++ apps/frontend/src/pages/AboutMePage.tsx
@@ -1,5 +1,9 @@
-import { type JSX } from "react";
+import { type JSX, useEffect, useState } from "react";
+import React from "react";
 import Page from "./Page";
+
+const GITHUB_LINK: string = "https://github.com/TechnikTil";
+const BIRTHDAY: number = Date.UTC(2010, 8, 24);
 
 export default class AboutMePage extends Page
 {
@@ -9,10 +13,92 @@ export default class AboutMePage extends Page
 
 	render(): JSX.Element
 	{
+		const fnf: JSX.Element = <a href="https://www.newgrounds.com/portal/view/770371">fnf</a>;
+
 		return (
 			<div>
-				<h1>Hello World! This is About Me.</h1>
+				<ul style={{marginTop: 10, fontSize: 25}}>
+					<this.GetAge />
+					<li>
+						i liek <span className="yellow">yellowe</span> :)
+					</li>
+					<li>Born in Germany, moved to Canada in 2020!</li>
+					<li>
+						<a href={GITHUB_LINK}>i code</a> (shocker)
+					</li>
+					<this.GetTimeZone />
+					<li>i try to draw, but suck at it</li>
+					<li>
+						i made music <span style={{fontSize: 30}}>once</span>
+					</li>
+					<li>{fnf} is love, {fnf} is life</li>
+					<li>will probably be a basement dweller for a living</li>
+					<li>
+						In a Discord Group Chat called "
+						<span style={{color: "#F58700"}}>Couples Counseling #1</span>" (dont ask)
+					</li>
+				</ul>
 			</div>
 		);
+	}
+
+	GetAge(): JSX.Element
+	{
+		const date: Date = new Date(BIRTHDAY);
+		const timeSinceDate: Date = new Date(Date.now() - BIRTHDAY);
+
+		var age: number = timeSinceDate.getUTCFullYear() - 1970;
+		var birthday: string = date.toLocaleDateString("en-CA", {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			timeZone: "UTC",
+		});
+		return <li>{age} years old ({birthday})</li>;
+	}
+
+	GetTimeZone(): JSX.Element
+	{
+		const [timeZoneData, setTimeZoneData] = useState<any>(undefined);
+		const [now, setNow] = useState<Date>(new Date());
+
+		useEffect(() =>
+		{
+			if (timeZoneData != undefined) return;
+
+			const updateTimeZone: () => Promise<void> = async () =>
+			{
+				const response: Response = await fetch("/api/timezone");
+				let data: any = await response.json();
+				setTimeZoneData(data);
+			};
+
+			updateTimeZone();
+		}, [timeZoneData]);
+
+		useEffect(() =>
+		{
+			const milisecondsElapsed: number = now.getUTCMilliseconds();
+			const timer: number = window.setInterval(() =>
+			{
+				setNow(new Date());
+			}, 1000 - milisecondsElapsed);
+
+			return () => clearInterval(timer);
+		});
+
+		if (timeZoneData == undefined) return <React.Fragment />;
+
+		const date: Date = new Date(now);
+		date.setUTCMinutes(date.getUTCMinutes() - timeZoneData.offset);
+
+		const timeZoneName: string = timeZoneData.name;
+		const timeDisplay: string = date.toLocaleTimeString("en-US", {
+			hour: "numeric",
+			minute: "numeric",
+			second: "numeric",
+			timeZone: "UTC",
+		});
+		return <li>My Timezone is {timeZoneName}! (so its {timeDisplay} for me)</li>;
 	}
 }
